c35ab93e53d6986c0169e748d1c5713202e39d92
Majid Ghaffuri <majidkabir@gmail.com>
2016-09-05 16:31:01 +0430

    After chapter "Models, Templates and Views"

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Import the Category model
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -21,7 +22,7 @@ def show_category(request, category_name_slug):
     context_dict = {}
 
     try:
-        category = Category.objects.filter(slug=category_name_slug)
+        category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
@@ -30,3 +31,41 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', context={'form':form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request,category.slug)
+        else:
+            form.errors
+
+    context_dict = {'form' : form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
